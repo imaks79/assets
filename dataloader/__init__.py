9bed6f00a021fc7a0ecfa20d8239cd294c646d6c
imaks79 <imaks9990@gmail.com>
2023-01-23 15:10:01 +0500

    Comment

--- dataloader/__init__.py
+++ dataloader/__init__.py
@@ -1,7 +1,7 @@
 from .parse_lvds import *
 
-from .parse_tlv import *
-from .analysis_tlv import *
-from .utils import *
-from .parse_cfg import * 
-from .setting_ports import *
+# from .parse_tlv import *
+# from .analysis_tlv import *
+# from .utils import *
+# from .parse_cfg import * 
+# from .setting_ports import *
